08f93a410d6b32a7d8ea573e701d4bd4ed6cfc86
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2026-07-26 08:49:19 -0300

    Criação das camadas

---
+++ RAG/RAG.Api/Controllers/RagController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RAG.Api.Controllers
+{
+    public class RagController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
